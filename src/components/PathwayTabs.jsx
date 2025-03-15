87a7d79d5ce31f5c01960d43ecf6a6feb3648f8b
Hakam aldeen Alkhadraa <hakamha8@gmail.com>
2025-03-16 00:04:46 +0300

    remove bottom rounded from tabs

--- src/components/PathwayTabs.jsx
+++ src/components/PathwayTabs.jsx
@@ -5,7 +5,7 @@ const PathwayTabs = ({ activeTab, onTabChange }) => {
         <button
           className={`px-4 py-2 text-sm rounded ${
             activeTab === "all"
-              ? "bg-[#F1F5F9] text-black"
+              ? "bg-[#F1F5F9] text-black rounded-b-none"
               : "bg-[#F3F3F3] text-[#484848] scale-[0.9]"
           }`}
           onClick={() => onTabChange("all")}
@@ -16,7 +16,7 @@ const PathwayTabs = ({ activeTab, onTabChange }) => {
         <button
           className={`px-4 py-2 text-sm rounded ${
             activeTab === "my"
-              ? "bg-[#F1F5F9] text-black"
+              ? "bg-[#F1F5F9] text-black rounded-b-none"
               : "bg-[#F3F3F3] text-[#484848] scale-[0.9]"
           }`}
           onClick={() => onTabChange("my")}
