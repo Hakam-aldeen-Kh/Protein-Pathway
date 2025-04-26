f97716851c430cba612bcc5a06edfc3a9c9f14be
Hakam aldeen Alkhadraa <hakamha8@gmail.com>
2025-04-26 13:09:01 +0300

    Use the same APIs for "Cell Type" & "Cellular Location"

--- src/common/reaction/sections/controllers/ControllerForm.jsx
+++ src/common/reaction/sections/controllers/ControllerForm.jsx
@@ -1,46 +1,132 @@
 import FormElement from "../../components/FormElement";
 
-const ControllerForm = ({ handleChangeData, reaction, controllerData, controllerIndex }) => {
+const ControllerForm = ({
+  handleChangeData,
+  reaction,
+  controllerData,
+  controllerIndex,
+}) => {
+  const handleChange = (e) =>
+    handleChangeData(e, reaction.id, "controllers", controllerIndex);
 
-    const handleChange = (e) => handleChangeData(e, reaction.id, "controllers", controllerIndex)
+  return (
+    <div className="space-y-4 p-4">
+      <div className="grid grid-cols-2 gap-4">
+        {/* <FormElement
+          isRequired={false}
+          type="select"
+          label={"Cell Type"}
+          name="cellType"
+          value={controllerData?.cellType}
+          handleChange={handleChange}
+          placeholder="Select Cell Type"
+          >
+          <option value="embryonic cell">embryonic cell</option>
+          <option value="prokaryotic cell">prokaryotic cell</option>
+        </FormElement> */}
 
-    return (
-        <div className="space-y-4 p-4">
-            <div className="grid grid-cols-2 gap-4">
-                <FormElement isRequired={false} type="select" label={"Cell Type"} name="cellType" value={controllerData?.cellType} handleChange={handleChange} placeholder="Select Cell Type">
-                    <option value="embryonic cell">embryonic cell</option>
-                    <option value="prokaryotic cell">prokaryotic cell</option>
-                </FormElement>
+        <FormElement
+          isRequired={false}
+          label={"Cell Type"}
+          name="cellType"
+          value={controllerData?.cellType}
+          handleChange={handleChange}
+          placeholder="Select Cell Type"
+          type="itemType"
+          itemType="CellType"
+        />
 
-                <FormElement isRequired={false} type="select" label={"Cellular Location"} name="cellularLocation" value={controllerData?.cellularLocation?.cell_localization_name} handleChange={handleChange} placeholder="Select Location" >
-                    <option value="cytocol">Cytosol</option>
-                    <option value="golgi">Golgi</option>
-                </FormElement>
-            </div>
+        {/* <FormElement
+          isRequired={false}
+          type="select"
+          label={"Cellular Location"}
+          name="cellularLocation"
+          value={controllerData?.cellularLocation?.cell_localization_name}
+          handleChange={handleChange}
+          placeholder="Select Location"
+        >
+          <option value="cytocol">Cytosol</option>
+          <option value="golgi">Golgi</option>
+        </FormElement> */}
 
-            <div className="grid grid-cols-2 gap-4">
-                <FormElement isRequired={false} type="select" label={"Controller Type"} name="controllerType" value={controllerData?.controllerType} handleChange={handleChange} placeholder="Select Controller Type">
-                    <option value="protein">Protein</option>
-                    <option value="enzyme">Enzyme</option>
-                </FormElement>
+        <FormElement
+          isRequired={false}
+          type="itemType"
+          label={" Cellular Location"}
+          name="cellularLocation"
+          value={controllerData?.cellularLocation?.cell_localization_name}
+          handleChange={handleChange}
+          placeholder="Select Location"
+          itemType="Cellular"
+        />
+      </div>
 
-                <FormElement isRequired={false} type="select" label={"Action Type"} name="actionType" value={controllerData?.actionType} handleChange={handleChange} placeholder="Select Action Type" >
-                    <option value="activation">Activation</option>
-                    <option value="inhibition">Inhibition</option>
-                </FormElement>
-            </div>
+      <div className="grid grid-cols-2 gap-4">
+        <FormElement
+          isRequired={false}
+          type="select"
+          label={"Controller Type"}
+          name="controllerType"
+          value={controllerData?.controllerType}
+          handleChange={handleChange}
+          placeholder="Select Controller Type"
+        >
+          <option value="protein">Protein</option>
+          <option value="enzyme">Enzyme</option>
+        </FormElement>
 
-            <div className="grid grid-cols-2 gap-4">
-                <FormElement isRequired={false} type="input" label={"When your complex is in GO ontology complex."} name="goOntology" value={controllerData?.goOntology} handleChange={handleChange} placeholder="" relatedInput={{ name: "goOntologyValue", value: controllerData?.goOntologyValue }} />
-                <FormElement isRequired={false} type="input" label={"When your complex is not in GO ontology complex."} name="notGoOntology" value={controllerData?.notGoOntology} handleChange={handleChange} placeholder="" />
-            </div>
+        <FormElement
+          isRequired={false}
+          type="select"
+          label={"Action Type"}
+          name="actionType"
+          value={controllerData?.actionType}
+          handleChange={handleChange}
+          placeholder="Select Action Type"
+        >
+          <option value="activation">Activation</option>
+          <option value="inhibition">Inhibition</option>
+        </FormElement>
+      </div>
 
-            <div className="grid grid-cols-2 gap-4">
-                <FormElement isRequired={false} type="checkbox" id={`useNextReactionController-${reaction.id}-${controllerIndex}`} placeholder={"Use this Controller as a reactant in the next reaction"} name="useNextReaction" value={controllerData?.useNextReaction} handleChange={handleChange} />
-            </div>
+      <div className="grid grid-cols-2 gap-4">
+        <FormElement
+          isRequired={false}
+          type="input"
+          label={"When your complex is in GO ontology complex."}
+          name="goOntology"
+          value={controllerData?.goOntology}
+          handleChange={handleChange}
+          placeholder=""
+          relatedInput={{
+            name: "goOntologyValue",
+            value: controllerData?.goOntologyValue,
+          }}
+        />
+        <FormElement
+          isRequired={false}
+          type="input"
+          label={"When your complex is not in GO ontology complex."}
+          name="notGoOntology"
+          value={controllerData?.notGoOntology}
+          handleChange={handleChange}
+          placeholder=""
+        />
+      </div>
 
-        </div>
-    )
+      <div className="grid grid-cols-2 gap-4">
+        <FormElement
+          isRequired={false}
+          type="checkbox"
+          id={`useNextReactionController-${reaction.id}-${controllerIndex}`}
+          placeholder={"Use this Controller as a reactant in the next reaction"}
+          name="useNextReaction"
+          value={controllerData?.useNextReaction}
+          handleChange={handleChange}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ControllerForm;
--- src/common/reaction/sections/products/ProductForm.jsx
+++ src/common/reaction/sections/products/ProductForm.jsx
@@ -1,109 +1,278 @@
 import FormElement from "../../components/FormElement";
 
-const ProductForm = ({ handleChangeData, reaction, productData, productIndex, handleCheckboxChange }) => {
+const ProductForm = ({
+  handleChangeData,
+  reaction,
+  productData,
+  productIndex,
+  handleCheckboxChange,
+}) => {
+  const handleChange = (e) =>
+    handleChangeData(e, reaction.id, "products", productIndex);
 
-    const handleChange = (e) => handleChangeData(e, reaction.id, "products", productIndex)
-
-    const handleChangeWithConfirm = (e) => {
-        if (productData?.useNextReaction) {
-            handleChange(e)
-        }
-        else {
-            handleCheckboxChange(reaction.id, "products", productIndex, e, e.target.checked)
-        }
+  const handleChangeWithConfirm = (e) => {
+    if (productData?.useNextReaction) {
+      handleChange(e);
+    } else {
+      handleCheckboxChange(
+        reaction.id,
+        "products",
+        productIndex,
+        e,
+        e.target.checked
+      );
     }
+  };
 
-    const handleUseInNextReaction = (e) => {
-        handleChangeData(e, reaction.id, "products", productIndex)
-        const targetReactionId = productData.targetReactionId
+  const handleUseInNextReaction = (e) => {
+    handleChangeData(e, reaction.id, "products", productIndex);
+    const targetReactionId = productData.targetReactionId;
 
-        // reset isProduct property in reactant and controller  
-        handleChangeData({ target: { value: false, name: "isProduct" } }, targetReactionId, "reactants", 0)
-        handleChangeData({ target: { value: false, name: "isProduct" } }, targetReactionId, "controllers", 0)
+    // reset isProduct property in reactant and controller
+    handleChangeData(
+      { target: { value: false, name: "isProduct" } },
+      targetReactionId,
+      "reactants",
+      0
+    );
+    handleChangeData(
+      { target: { value: false, name: "isProduct" } },
+      targetReactionId,
+      "controllers",
+      0
+    );
 
-        // reset reference property in reactant and controller  
-        handleChangeData({ target: { value: "", name: "reference" } }, targetReactionId, "reactants", 0)
-        handleChangeData({ target: { value: "", name: "reference" } }, targetReactionId, "controllers", 0)
+    // reset reference property in reactant and controller
+    handleChangeData(
+      { target: { value: "", name: "reference" } },
+      targetReactionId,
+      "reactants",
+      0
+    );
+    handleChangeData(
+      { target: { value: "", name: "reference" } },
+      targetReactionId,
+      "controllers",
+      0
+    );
 
-        // reset fromReaction property in reactant and controller  
-        handleChangeData({ target: { value: "", name: "fromReaction" } }, targetReactionId, "reactants", 0)
-        handleChangeData({ target: { value: "", name: "fromReaction" } }, targetReactionId, "controllers", 0)
-        handleChangeData({ target: { value: "", name: "fromProduct" } }, targetReactionId, "reactants", 0)
-        handleChangeData({ target: { value: "", name: "fromProduct" } }, targetReactionId, "controllers", 0)
-        //change reference and isProduct property in reactant or controller  
-        if (e.target.value) {
-            handleChangeData({ target: { value: true, name: "isProduct" } }, targetReactionId, e.target.value, 0);
-            handleChangeData({ target: { value: `(Product - ${reaction.id}.${productData.id} of Reaction ${reaction.id})`, name: "reference" } }, targetReactionId, e.target.value, 0)
-            handleChangeData({ target: { value: reaction.id, name: "fromReaction" } }, targetReactionId, e.target.value, 0)
-            handleChangeData({ target: { value: productData.id, name: "fromProduct" } }, targetReactionId, e.target.value, 0)
-
-        }
-
-
+    // reset fromReaction property in reactant and controller
+    handleChangeData(
+      { target: { value: "", name: "fromReaction" } },
+      targetReactionId,
+      "reactants",
+      0
+    );
+    handleChangeData(
+      { target: { value: "", name: "fromReaction" } },
+      targetReactionId,
+      "controllers",
+      0
+    );
+    handleChangeData(
+      { target: { value: "", name: "fromProduct" } },
+      targetReactionId,
+      "reactants",
+      0
+    );
+    handleChangeData(
+      { target: { value: "", name: "fromProduct" } },
+      targetReactionId,
+      "controllers",
+      0
+    );
+    //change reference and isProduct property in reactant or controller
+    if (e.target.value) {
+      handleChangeData(
+        { target: { value: true, name: "isProduct" } },
+        targetReactionId,
+        e.target.value,
+        0
+      );
+      handleChangeData(
+        {
+          target: {
+            value: `(Product - ${reaction.id}.${productData.id} of Reaction ${reaction.id})`,
+            name: "reference",
+          },
+        },
+        targetReactionId,
+        e.target.value,
+        0
+      );
+      handleChangeData(
+        { target: { value: reaction.id, name: "fromReaction" } },
+        targetReactionId,
+        e.target.value,
+        0
+      );
+      handleChangeData(
+        { target: { value: productData.id, name: "fromProduct" } },
+        targetReactionId,
+        e.target.value,
+        0
+      );
     }
+  };
 
+  return (
+    <div className="space-y-4 p-4">
+      <div className="grid grid-cols-2 gap-4">
+        {/* <FormElement
+          isRequired={false}
+          type="select"
+          label={"Cell Type"}
+          name="cellType"
+          value={productData?.cellType}
+          handleChange={handleChange}
+          placeholder="Select Cell Type"
+        >
+          <option value="embryonic cell">embryonic cell</option>
+          <option value="prokaryotic cell">prokaryotic cell</option>
+        </FormElement> */}
 
-    return (
-        <div className="space-y-4 p-4">
+        <FormElement
+          isRequired={false}
+          label={"Cell Type"}
+          name="cellType"
+          value={productData?.cellType}
+          handleChange={handleChange}
+          placeholder="Select Cell Type"
+          type="itemType"
+          itemType="CellType"
+        />
 
-            <div className="grid grid-cols-2 gap-4">
-                <FormElement isRequired={false} type="select" label={"Cell Type"} name="cellType" value={productData?.cellType} handleChange={handleChange} placeholder="Select Cell Type">
-                    <option value="embryonic cell">embryonic cell</option>
-                    <option value="prokaryotic cell">prokaryotic cell</option>
-                </FormElement>
+        {/* <FormElement
+          isRequired={false}
+          type="select"
+          label={" Cellular Location"}
+          name="cellularLocation"
+          value={productData?.cellularLocation}
+          handleChange={handleChange}
+          placeholder="Select Location"
+        >
+          <option value="cytocol">Cytosol</option>
+          <option value="golgi">Golgi</option>
+        </FormElement> */}
 
-                <FormElement isRequired={false} type="select" label={" Cellular Location"} name="cellularLocation" value={productData?.cellularLocation} handleChange={handleChange} placeholder="Select Location" >
-                    <option value="cytocol">Cytosol</option>
-                    <option value="golgi">Golgi</option>
-                </FormElement>
-            </div>
+        <FormElement
+          isRequired={false}
+          type="itemType"
+          label={" Cellular Location"}
+          name="cellularLocation"
+          value={productData?.cellularLocation}
+          handleChange={handleChange}
+          placeholder="Select Location"
+          itemType="Cellular"
+        />
+      </div>
 
-            <div className="grid grid-cols-2 gap-4">
-                <FormElement isRequired={false} type="select" label={"Product Type"} name="productType" value={productData?.productType} handleChange={handleChange} placeholder="Select Product Type">
-                    <option value="protein">Protein</option>
-                    <option value="complex">Complex</option>
-                </FormElement>
-            </div>
+      <div className="grid grid-cols-2 gap-4">
+        <FormElement
+          isRequired={false}
+          type="select"
+          label={"Product Type"}
+          name="productType"
+          value={productData?.productType}
+          handleChange={handleChange}
+          placeholder="Select Product Type"
+        >
+          <option value="protein">Protein</option>
+          <option value="complex">Complex</option>
+        </FormElement>
+      </div>
 
-            <div>
-                <span className=' font-bold text-xs block py-4'>Protein Name</span>
+      <div>
+        <span className=" font-bold text-xs block py-4">Protein Name</span>
 
-                <div className="grid grid-cols-2 gap-4">
-                    <FormElement isRequired={false} type="input" label={"Binding site Code"} name="bindingSiteCode" value={productData?.bindingSiteCode} handleChange={handleChange} placeholder="Three letters code of binding site (e.g. ser, tyr...)" />
-                    <FormElement isRequired={false} type="input" label={"Protein Symbol"} name="proteinSymbol" value={productData?.proteinSymbol} handleChange={handleChange} placeholder="Type protein symbol" />
-                </div>
-            </div>
+        <div className="grid grid-cols-2 gap-4">
+          <FormElement
+            isRequired={false}
+            type="input"
+            label={"Binding site Code"}
+            name="bindingSiteCode"
+            value={productData?.bindingSiteCode}
+            handleChange={handleChange}
+            placeholder="Three letters code of binding site (e.g. ser, tyr...)"
+          />
+          <FormElement
+            isRequired={false}
+            type="input"
+            label={"Protein Symbol"}
+            name="proteinSymbol"
+            value={productData?.proteinSymbol}
+            handleChange={handleChange}
+            placeholder="Type protein symbol"
+          />
+        </div>
+      </div>
 
-            <div>
-                <span className=' font-bold text-xs block py-4'>Protein size (Fragmented protein size information)</span>
+      <div>
+        <span className=" font-bold text-xs block py-4">
+          Protein size (Fragmented protein size information)
+        </span>
 
-                <div className="grid grid-cols-2 gap-4">
-                    <FormElement isRequired={false} type="input" label={"Starting Site"} name="startingSite" value={productData?.startingSite} handleChange={handleChange} placeholder="Starting Site" />
-                    <FormElement isRequired={false} type="input" label={"Ending Site"} name="endingSite" value={productData?.endingSite} handleChange={handleChange} placeholder="Ending Site" />
-                </div>
-            </div>
+        <div className="grid grid-cols-2 gap-4">
+          <FormElement
+            isRequired={false}
+            type="input"
+            label={"Starting Site"}
+            name="startingSite"
+            value={productData?.startingSite}
+            handleChange={handleChange}
+            placeholder="Starting Site"
+          />
+          <FormElement
+            isRequired={false}
+            type="input"
+            label={"Ending Site"}
+            name="endingSite"
+            value={productData?.endingSite}
+            handleChange={handleChange}
+            placeholder="Ending Site"
+          />
+        </div>
+      </div>
 
-            <div className="grid grid-cols-2 gap-4">
-                <FormElement isRequired={false} type="checkbox" id={`useNextReaction1-${reaction.id}-${productIndex}`} placeholder={"Use this product as a reactant in the next reaction"} name="useNextReaction" value={productData?.useNextReaction} handleChange={handleChangeWithConfirm} />
-            </div>
+      <div className="grid grid-cols-2 gap-4">
+        <FormElement
+          isRequired={false}
+          type="checkbox"
+          id={`useNextReaction1-${reaction.id}-${productIndex}`}
+          placeholder={"Use this product as a reactant in the next reaction"}
+          name="useNextReaction"
+          value={productData?.useNextReaction}
+          handleChange={handleChangeWithConfirm}
+        />
+      </div>
 
-            {productData.useNextReaction &&
-                <div className="grid grid-cols-2 gap-4">
-                    <FormElement isRequired={false} type="select" label={"Type"} name="type" value={productData?.type} handleChange={handleUseInNextReaction} placeholder="Type">
-                        <option value="reactants">Ractant</option>
-                        <option value="controllers">Controller</option>
-                    </FormElement>
+      {productData.useNextReaction && (
+        <div className="grid grid-cols-2 gap-4">
+          <FormElement
+            isRequired={false}
+            type="select"
+            label={"Type"}
+            name="type"
+            value={productData?.type}
+            handleChange={handleUseInNextReaction}
+            placeholder="Type"
+          >
+            <option value="reactants">Ractant</option>
+            <option value="controllers">Controller</option>
+          </FormElement>
 
-                    <div>
-                        <label className="block text-sm font-medium text-gray-700">Targeted Reaction</label>
-                        <input
-                            disabled
-                            className="mt-1 border block w-full rounded-md p-2 border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
-                            value={`Reaction - ${productData?.targetReactionId}`}
-                        />
-                    </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Targeted Reaction
+            </label>
+            <input
+              disabled
+              className="mt-1 border block w-full rounded-md p-2 border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+              value={`Reaction - ${productData?.targetReactionId}`}
+            />
+          </div>
 
-                    {/* <div>
+          {/* <div>
                         <label className="block text-sm font-medium text-gray-700">Targeted Reaction</label>
                         <select
                             disabled={!productData.type}
@@ -118,12 +287,10 @@ const ProductForm = ({ handleChangeData, reaction, productData, productIndex, ha
                             )}
                         </select>
                     </div> */}
-
-
-                </div>
-            }
         </div>
-    )
+      )}
+    </div>
+  );
 };
 
 export default ProductForm;
